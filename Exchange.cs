dfb561e077392c000876a2dec71998ec125a870c
Vkorzh <vladyss55@gmail.com>
2018-11-06 23:14:33 +0300

    Initial commit

---
+++ Exchange.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exchange
+{
+  
+class Program
+    {
+        static double sum(double a, double b)
+        {
+            return a + b;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите первое число");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите второе число");
+            double b = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Первое чилсо равняется {0}",a);
+            Console.WriteLine("Второе чилсо равняется {0}", b);
+            Console.WriteLine("Потвердите выполнение метода");
+            Console.ReadLine();
+            a = (sum(a, b) - a);
+            b = (sum(a, b) - b);
+            Console.WriteLine("После выполнения функции");
+            Console.WriteLine("Первое чилсо равняется {0}", a);
+            Console.WriteLine("Второе чилсо равняется {0}", b);
+            Console.ReadLine();
+        }
+}
+}
